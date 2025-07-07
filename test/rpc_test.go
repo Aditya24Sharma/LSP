373a5daed012e44a50a36db9a4555d654ea830e0
Aditya Sharma <aditya.sharma@usm.edu>
2025-07-07 12:22:41 -0500

    Decode Message and Test

--- test/rpc_test.go
+++ test/rpc_test.go
@@ -16,3 +16,15 @@ func TestEncode(t *testing.T) {
 		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
 	}
 }
+
+func TestDecode(t *testing.T) {
+	incoming := ("Content-Length: 16\r\n\r\n{\"Testing\":true}")
+	contentLength, err := rpc.DecodeMessage([]byte(incoming))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if contentLength != 16 {
+		t.Fatalf("Content length should be 16 but got %d", contentLength)
+	}
+}
